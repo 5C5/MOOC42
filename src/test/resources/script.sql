f64b2169689c6248375d3708d6a8409a2375397f
5c5 <colas.p.s@gmail.com>
2016-04-24 15:15:58 +0200

    ajout du script sql

---
+++ src/test/resources/script.sql
@@ -0,0 +1,33 @@
+CREATE TABLE	 apprenant (
+	id INT PRIMARY KEY NOT NULL,
+	prenom VARCHAR(50) NOT NULL,
+	nom VARCHAR(50)
+	)
+	
+INSERT INTO apprenant VALUES (
+	011, 	'COLAS', 	'PICARD')
+	
+CREATE TABLE  connaissances(
+	id_apprenant INT PRIMARY KEY NOT NULL,
+	id_notion INT
+	)
+	
+ALTER TABLE connaissances ALTER COLUMN id_notion SET NOT NULL;
+	
+CREATE TABLE notions (
+	id INT PRIMARY KEY NOT NULL,
+	nom VARCHAR(4)
+	)
+	
+INSERT INTO notions VALUES
+(01, 'OU'),
+(02, 'AND');
+	
+CREATE TABLE competences (
+	id_apprenant INT PRIMARY KEY NOT NULL,
+	id_notion INT,
+	difficulte INT,
+	score INT
+	)
+	
+ALTER TABLE competences ALTER COLUMN id_notion SET NOT NULL;
