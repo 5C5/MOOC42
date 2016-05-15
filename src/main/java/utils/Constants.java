e5dcdbd3bbaf584bde7d5864debcfdc3dd13d2c7
5c5 <colas.p.s@gmail.com>
2016-05-15 15:38:50 +0200

    temp commit

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -20,10 +20,30 @@ public final class Constants {
     /** Constante NON */
     public static final String NON = "non";
 
+	/**
+	 * ********************* Portes Logiques ******************
+	 */
+	/** Et */
+	public static final String AND = "AND";
+	/** OU */
+	public static final String OR = "OR";
+	/** Negation */
+	public static final String NOT = "NOT";
+	/** Ou exclusif */
+	public static final String XOR = "XOR";
+	/** Vide, ne fait rien */
+	public static final String EMPTY = "EMPTY";
+
+	public static boolean isLogicGate(String gate) {
+
+		return (gate == AND || gate == OR || gate == NOT || gate == XOR || gate == EMPTY || gate == "");
+	}
+
     /**
      * Non instantiable
      */
     private Constants() {
     }
+
 
 }
